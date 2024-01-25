504e16fcdd8af9c59eb672e8ac9a4ec949a702db
sixmillions <liubw95@163.com>
2024-01-25 16:42:26 +0800

    Init

---
+++ src/main/java/cn/sixmillions/blog/NextBlogApiApplication.java
@@ -0,0 +1,13 @@
+package cn.sixmillions.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextBlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/sixmillions/blog/NextBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sixmillions.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
